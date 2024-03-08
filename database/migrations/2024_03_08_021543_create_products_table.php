cc02e4a6eaf44afd7e190c3d98044962d0c4675c
EronAlves1996 <eron.alves@rocketmail.com>
2024-03-07 23:30:40 -0300

    feat: create product infrastructure

---
+++ database/migrations/2024_03_08_021543_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->uuid("public_id");
+            $table->string("name", 100);
+            $table->integer("quantity")->unsigned();
+            $table->string("picture_filename")->nullable(true);
+            $table->bigInteger("price")->unsigned();
+            $table->timestamps();
+
+            $table->unique("name", "unique_name");
+            $table->unique("public_id", "unique_public_id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
