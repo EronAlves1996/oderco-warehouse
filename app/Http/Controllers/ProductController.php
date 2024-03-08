cc90851ccb0c4a2ffc1d26b9e9c6997cd15afd00
EronAlves1996 <eron.alves@rocketmail.com>
2024-03-07 23:59:02 -0300

    refactor: use laravel helper method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,14 +15,6 @@ class ProductController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
