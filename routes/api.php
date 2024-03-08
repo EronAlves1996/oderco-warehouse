602d1a7dbe9af72d7a930dc4d07f58ac707decb0
EronAlves1996 <eron.alves@rocketmail.com>
2024-03-08 00:35:14 -0300

    fix: api endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/product', function () {
+Route::prefix('/products')->group(function () {
     Route::post('/', [ProductController::class, 'store']);
 });
