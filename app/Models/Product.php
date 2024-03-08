81ecc54d3c0976e523b6534ed6eb8f65e2931099
EronAlves1996 <eron.alves@rocketmail.com>
2024-03-07 23:34:55 -0300

    feat: autoassign uuid field

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
 
 class Product extends Model
 {
     use HasFactory;
 
-    protected $table = "product";
+    protected $table = 'product';
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($model) {
+            $model->public_id = Uuid::uuid4();
+        });
+    }
 }
