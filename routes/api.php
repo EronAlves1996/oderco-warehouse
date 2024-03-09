3b8e2821e8b58f00747a29790a79a36998c7b620
EronAlves1996 <eron.alves@rocketmail.com>
2024-03-09 00:17:25 -0300

    feat: declares route for get a product

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/products')->group(function () {
     Route::post('/', [ProductController::class, 'store']);
+    Route::get('/{product:public_id}', [ProductController::class, 'show']);
 });
