9862f052bda582e3cd917f8cebc1f2f2bd7d866e
EronAlves1996 <eron.alves@rocketmail.com>
2024-03-10 01:18:26 -0300

    chore: main template

--- web/app.vue
+++ web/app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <header
+    class="d-flex align-items-center gap-5 mx-auto border-bottom border-3"
+  >
+    <MainLogo />
+    <input
+      type="text"
+      class="form-control w-75"
+      placeholder="Pesquisar..."
+      aria-label="Pesquisar..."
+    />
+  </header>
+  <slot />
 </template>
