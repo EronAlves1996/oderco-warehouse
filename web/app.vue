84428b84ad61655027f402eb6dde7f7f6ca99944
EronAlves1996 <eron.alves@rocketmail.com>
2024-03-27 23:05:17 -0300

    feat: go to home click on logo

--- web/app.vue
+++ web/app.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-const sectionTitle = ref("");
-provide("setTitle", (title: string) => (sectionTitle.value = title));
+  const sectionTitle = ref('');
+  provide('setTitle', (title: string) => (sectionTitle.value = title));
 </script>
 <template>
   <header
-    class="d-flex align-items-center gap-5 mx-auto border-bottom border-3"
-  >
-    <MainLogo />
+    class="d-flex align-items-center gap-5 mx-auto border-bottom border-3">
+    <NuxtLink to="/">
+      <MainLogo />
+    </NuxtLink>
     <input
       type="text"
       class="form-control w-75"
       placeholder="Pesquisar..."
-      aria-label="Pesquisar..."
-    />
+      aria-label="Pesquisar..." />
   </header>
   <section class="py-4 px-5 d-flex flex-column">
     <NuxtPage />
