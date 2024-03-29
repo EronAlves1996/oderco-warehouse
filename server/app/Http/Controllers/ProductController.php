ebb361c4d24ef6ddc561685cb42b3e349abb89ba
EronAlves1996 <eron.alves@rocketmail.com>
2024-03-29 11:47:33 -0300

    refactor: make revision of collocation of some operations

--- server/app/Http/Controllers/ProductController.php
+++ server/app/Http/Controllers/ProductController.php
@@ -2,20 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\ForbiddenOperationException;
+use App\Exceptions\UnexpectedDatabaseException;
 use App\Models\Product;
+use Exception;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
+    private static $base_validation = [
+        "quantity" => ["required", "numeric", "integer", "min:0"],
+        "image" => ["nullable", "extensions:jpg,png"],
+        "price" => ["required", "decimal:0,2", "min:0"],
+    ];
+
+    private static $name_validations = [
+        "update" => ["required", "max:100"],
+        "creation" => ["required", "unique:product,name", "max:100"],
+    ];
+
+    /**
+     * @param array<int,mixed> $requestArray
+     * @return array<int,mixed>|null
+     */
+    private static function saveImage(array $requestArray): array
+    {
+        if (array_key_exists("image", $requestArray)) {
+            $fileName = $requestArray["image"]->storePublicly();
+            $requestArray["picture"] = $fileName;
+        }
+        return $requestArray;
+    }
+
+    /**
+     * @param mixed $runnable
+     * @param mixed $recoverFunction
+     */
+    private static function safeExecute($runnable, $recoverFunction)
+    {
+        try {
+            return $runnable();
+        } catch (Exception $e) {
+            $recoverFunction();
+        }
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index(): LengthAwarePaginator
     {
-        return Product::query()->paginate(10);
+        return Product::query()->paginate(2);
     }
 
     /**
@@ -23,7 +64,23 @@ class ProductController extends Controller
      */
     public function store(Request $request): Response|ResponseFactory
     {
-        $createdProduct = Product::newFromRequest($request);
+        $product = $request->validate(
+            array_merge(static::$base_validation, [
+                "name" => static::$name_validations["creation"],
+            ])
+        );
+
+        $productWithImage = static::saveImage($product);
+
+        $createdProduct = static::safeExecute(
+            fn() => Product::query()->create($productWithImage),
+            function () use ($productWithImage) {
+                if (array_key_exists("picture", $productWithImage)) {
+                    Storage::delete($productWithImage["picture"]);
+                }
+                throw new UnexpectedDatabaseException();
+            }
+        );
 
         return response(null, 201, [
             "location" =>
@@ -44,7 +101,34 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product): void
     {
-        $product->updateFromRequest($request);
+        if ($request->input("public_id") !== $this->public_id) {
+            throw new ForbiddenOperationException(
+                "Não é possível atualizar uma entidade diferente desta!"
+            );
+        }
+
+        $toUpdate = $request->validate(
+            array_merge(
+                static::$base_validation,
+                ["name" => static::$name_validations["update"]],
+                static::$public_id_validation
+            )
+        );
+
+        $toUpdateWithImage = static::saveImage($toUpdate);
+
+        static::safeExecute(
+            function () use ($toUpdateWithImage, $product) {
+                $product->fill($toUpdateWithImage);
+                $product->save();
+            },
+            function () use ($toUpdateWithImage) {
+                if (array_key_exists("picture", $toUpdateWithImage)) {
+                    Storage::delete($toUpdateWithImage["picture"]);
+                }
+                throw new UnexpectedDatabaseException();
+            }
+        );
     }
 
     /**
--- server/app/Models/Product.php
+++ server/app/Models/Product.php
@@ -2,15 +2,10 @@
 
 namespace App\Models;
 
-use App\Exceptions\ForbiddenOperationException;
-use App\Exceptions\UnexpectedDatabaseException;
-use Exception;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 use Ramsey\Uuid\Uuid;
 
 class Product extends Model
@@ -30,17 +25,6 @@ class Product extends Model
     protected $hidden = ["id", "deleted_at"];
 
     protected $appends = ["price", "picture"];
-
-    private static $name_validations = [
-        "update" => ["required", "max:100"],
-        "creation" => ["required", "unique:product,name", "max:100"],
-    ];
-
-    private static $base_validation = [
-        "quantity" => ["required", "numeric", "integer", "min:0"],
-        "image" => ["nullable", "extensions:jpg,png"],
-        "price" => ["required", "decimal:0,2", "min:0"],
-    ];
 
     private static $public_id_validation = [
         "public_id" => ["required", "uuid"],
@@ -73,84 +57,5 @@ class Product extends Model
                 ? ""
                 : asset("/storage/" . $value)
         )->shouldCache();
-    }
-
-    public static function newFromRequest(Request $request): Product
-    {
-        $product = $request->validate(
-            array_merge(static::$base_validation, [
-                "name" => static::$name_validations["creation"],
-            ])
-        );
-
-        $productWithImage = static::saveImage($product);
-
-        return static::safeExecute(
-            fn() => Product::query()->create($productWithImage),
-            function () use ($productWithImage) {
-                if (array_key_exists("picture", $productWithImage)) {
-                    Storage::delete($productWithImage["picture"]);
-                }
-                throw new UnexpectedDatabaseException();
-            }
-        );
-    }
-    /**
-     * @param mixed $runnable
-     * @param mixed $recoverFunction
-     */
-    private static function safeExecute($runnable, $recoverFunction)
-    {
-        try {
-            return $runnable();
-        } catch (Exception $e) {
-            $recoverFunction();
-        }
-    }
-
-    /**
-     * @param array<int,mixed> $requestArray
-     * @return array<int,mixed>|null
-     */
-    private static function saveImage(array $requestArray): array
-    {
-        if (array_key_exists("image", $requestArray)) {
-            $fileName = $requestArray["image"]->storePublicly();
-            $requestArray["picture"] = $fileName;
-        }
-        return $requestArray;
-    }
-
-    public function updateFromRequest(Request $request): void
-    {
-        if ($request->input("public_id") !== $this->public_id) {
-            throw new ForbiddenOperationException(
-                "Não é possível atualizar uma entidade diferente desta!"
-            );
-        }
-
-        $toUpdate = $request->validate(
-            array_merge(
-                static::$base_validation,
-                ["name" => static::$name_validations["update"]],
-                static::$public_id_validation
-            )
-        );
-
-        $toUpdateWithImage = static::saveImage($toUpdate);
-
-        var_dump($toUpdateWithImage);
-        static::safeExecute(
-            function () use ($toUpdateWithImage) {
-                $this->fill($toUpdateWithImage);
-                $this->save();
-            },
-            function () use ($toUpdateWithImage) {
-                if (array_key_exists("picture", $toUpdateWithImage)) {
-                    Storage::delete($toUpdateWithImage["picture"]);
-                }
-                throw new UnexpectedDatabaseException();
-            }
-        );
     }
 }
