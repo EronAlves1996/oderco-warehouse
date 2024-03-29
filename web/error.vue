9b55a0775f7e7903f01b0a82b8981e997864b889
EronAlves1996 <eron.alves@rocketmail.com>
2024-03-29 13:20:36 -0300

    feat: personalized error page

---
+++ web/error.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import type { NuxtError } from '#app';
+
+  const props = defineProps({
+    error: Object as () => NuxtError,
+  });
+
+  const handleError = () => clearError({ redirect: '/' });
+</script>
+<template>
+  <div
+    class="w-100 d-flex align-items-center justify-content-center flex-column gap-3">
+    <MainLogo />
+    <h1>{{ error?.statusCode }}</h1>
+    <h3>{{ error?.statusMessage }}</h3>
+    <DefaultButton
+      @click="handleError"
+      type="button">
+      Voltar para página principal
+    </DefaultButton>
+  </div>
+</template>
