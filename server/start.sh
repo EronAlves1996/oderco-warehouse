c650f5aef09d671b014e9deb5a5dea693ba865a9
EronAlves1996 <eron.alves@rocketmail.com>
2024-03-31 19:15:44 -0300

    build: adjust build

--- server/start.sh
+++ server/start.sh
@@ -1,14 +1,15 @@
-php artisan migrate
+php artisan config:clear
+php artisan route:clear
 
 # Migrações só ocorrem sob ENV local
+export APP_ENV=local
+
+php artisan migrate
+
 export APP_ENV=production
 
-php artisan config:clear
 php artisan config:cache
-
-php artisan route:clear
 php artisan route:cache
-
 php artisan optimize
 
 php-fpm
